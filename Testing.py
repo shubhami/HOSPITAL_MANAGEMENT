8b889450dc3fd47382ac9db3260dedb61b53bb68
shubhami <pareekshubham59@gmail.com>
2022-07-17 20:13:24 +0530

    Cleared cache to ignore the files mentioned in gitignore

--- Testing.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from sqlalchemy import create_engine
-
-engine = create_engine("sqlite:///patient_record.db")
-
-#to load entire details table
-df=pd.read_sql("details",engine)
-
-print(df.head())
